5e929b25e374d1298a2876ddc3635440ae96953f
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-08-01 19:21:19 +0530

    commit

---
+++ build_a_form_with_validation/README.md
@@ -0,0 +1,3 @@
+# build_a_form_with_validation
+
+A new Flutter project.
---
+++ build_a_form_with_validation/android/app/src/main/kotlin/com/example/build_a_form_with_validation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.build_a_form_with_validation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
